2b6c26637e810abc0a42826a1b867341f3c00808
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-04-05 15:56:35 +0400

    Add files via upload

---
+++ shortId.js
@@ -0,0 +1,11 @@
+const redis = require("redis");
+const client = redis.createClient();
+
+module.exports.generate = function () {
+  const id = Math.random().toString(36).substring(2, 9);
+  client.exists(id, (err, val) => {
+    if (err) throw err;
+    if (val != 0) return this.generate();
+  });
+  return id;
+};
