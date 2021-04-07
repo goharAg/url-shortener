e76dd8bdf16d2cb26784165d95b98f11d8d01a42
gohar <aghajanyangohar01@gmail.com>
2021-04-07 20:17:18 +0400

    changed port number

--- connections.js
+++ connections.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3003;
 
 //connecting to DB and Server
 module.exports.connect = async () => {
